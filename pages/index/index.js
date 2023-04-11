5622477f1c2a5095b1363f076b0301cd8f4101cd
Elkin Silva <elkinsilvamanrique@gmail.com>
2023-04-11 09:51:30 -0500

    Add comments

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,12 @@ var app = getApp();
 
 Page({
   onLoad() {
+    //Mostrar un spinner de carga
     my.showLoading({
       content: "Cargando..."
     });
 
+    //No mostrar el spinner luego de 2000ms
     setTimeout(() => {
       my.hideLoading();
     }, 2000);
@@ -15,13 +17,9 @@ Page({
 
   data: {
     nombre: app.globalData.nombre,
-
     fecha: app.globalData.fecha,
-
     edad: app.globalData.edad,
-
     google: false,
-
     facebook: false
   },
 
